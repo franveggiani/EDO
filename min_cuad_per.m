28ba3b0d90a31ec95772ef20c8d35244f68d79ce
franveggiani <francoveggiani@gmail.com>
2023-10-09 21:02:39 -0300

    asdf

--- min_cuad_per.m
+++ min_cuad_per.m
@@ -15,9 +15,42 @@ function min_cuad_per
     figure(1)
     plot(t, g, 'or')
 
-    phi = calcular_matriz_phi(N, dim);
+    phi = calcular_matriz_phi(N, dim); % Llamada a función
 
-    display(phi);
+    % PHI' * PHI * a = PHI' * g
+
+    A = phi' * phi;
+    B = phi' * g;
+
+    display(A)
+
+    %Coeficientes (vector a)
+
+    a = inv(A) * B;
+
+    display(a);
+
+    %Amplitud
+
+    [tAmp, Amp] = calcular_amplitudes(dim, a); % Llamada a función
+
+    display(Amp');
+
+    %Gráfico de barras
+
+    figure(2)
+    bar(tAmp, Amp)
+
+    %Función contínua
+
+    dtc = 1/100;
+    ndt = 100;
+
+    P(1) = a(1);
+    t(1) = 0;
+
+    %P(i) =  cos(1*(w/N)*(i - t(1))) + sin(1*(w/N)*(i - t(1))) + cos(2*(w/N)*(i - t(1))) + sin(2*(w/N)*(i - t(1))) + cos(3*(w/N)*(i - t(1))) + sin(3*(w/N)*(i - t(1)));
+
 
 end
 
@@ -46,5 +79,27 @@ function [phi] = calcular_matriz_phi(N, dim)
             k = k + 1;
         end
     end
+
+end
+
+function [tAmp, Amp] = calcular_amplitudes(dim, a) % Devuelve el vector tAmp para poder graficar y el vector Amp que es el vector de amplitudes
+
+    Amp(1) = a(1); % A(0) = a0
+    tAmp(1) = 0;
+
+    contador = 2;
+
+    for i=2 : ((dim-1)/2) + 1 
+
+        x = a(contador); % Se obtiene elemento a (elementos pares del vector a)
+        y = a(contador + 1); % Se obtiene el elemento b (elementos impares del vector b)
+
+        Amp(i)=(x^2 + y^2)^(1/2); % Se calcula la amplitud
+
+        tAmp(i) = tAmp(i-1) + i; % Vector t para poder graficar
+
+        contador = contador + 2;
+
+    endfor
 
 end
